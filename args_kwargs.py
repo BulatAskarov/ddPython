51d7115912b854082441e5f5e1b611b6afe9d112
6yJlaTuk <askarov_bulat@mail.ru>
2022-10-12 12:18:58 +0300

    + args, kwargs

---
+++ args_kwargs.py
@@ -0,0 +1,9 @@
+def test(a, b, *args, is_test=True, **kwargs):
+    print("простые аргументы:", a, b)
+    print("args", args)
+    print("is test", is_test)
+    print("kwargs", kwargs)
+
+
+test(1, 2, 3, 4, 5, is_test=False, message="test message")
+
