c254c2969e0bcab1add96d66bbbb3a9d0d3e20e0
6yJlaTuk <askarov_bulat@mail.ru>
2022-10-12 12:55:35 +0300

    + modules

--- args_kwargs.py
+++ args_kwargs.py
@@ -1,3 +1,7 @@
+from translator.data import DICT
+from translator.service import get_translation
+
+
 def test(a, b, *args, is_test=True, **kwargs):
     print("простые аргументы:", a, b)
     print("args", args)
@@ -7,3 +11,19 @@ def test(a, b, *args, is_test=True, **kwargs):
 
 test(1, 2, 3, 4, 5, is_test=False, message="test message")
 
+def get_word_translation(*args, **kwargs):
+    print(args)
+    print(kwargs)
+    local_dict = dict(**DICT, **kwargs)
+    lst = [1, 2, 3]
+    test_lst = [*args, *lst]
+    print(test_lst)
+    results = []
+    for arg in args:
+        results.append(get_translation(arg, local_dict))
+    return results
+
+
+print(get_word_translation(
+    'son', 'mother', 'clock', clock='часы', key='ключ'
+))
---
+++ translator/service.py
@@ -0,0 +1,24 @@
+def get_translation(word, dictionary):
+    if word in dictionary:
+        translation = dictionary[word]
+        return dictionary[word]
+    else:
+        return None
+
+
+def prepare_word(word):
+    return word.lower().strip()
+
+
+def get_word_translation(*args, **kwargs):
+    print(args)
+    print(kwargs)
+    local_dict = dict(**DICT, **kwargs)
+    lst = [1, 2, 3]
+    test_lst = [*args, *lst]
+    print(test_lst)
+    results = []
+    for arg in args:
+        results.append(get_translation(arg, local_dict))
+    return results
+
