4e13349762bce5dfc35d8a232cc279984bdbdcca
6yJlaTuk <askarov_bulat@mail.ru>
2022-10-26 00:37:17 +0300

    + task3

---
+++ Tasks/find_max.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def io_find_max_decorator(func):
+    def wrapper(*args, **kwargs):
+        print(args)
+        print(func(*args, **kwargs))
+    return wrapper
+
+
+@io_find_max_decorator
+def find_max(lst):
+    m = 1 - sys.maxsize
+    for n in lst:
+        if n > m:
+            m = n
+    return m
+
+
+find_max([1, 2, 3])
+
